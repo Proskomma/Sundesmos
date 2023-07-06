8703236943d1732fe775a5ad3e006d1a9fec0ced
LeO <devgru0114@gmail.com>
2023-07-06 17:58:57 -0400

    fix: fix readUsfm

--- src/utils/readUsfm.ts
+++ src/utils/readUsfm.ts
@@ -215,20 +215,6 @@ export const readUsfm = (srcUsfm: string | undefined) => {
   const cl = new SofriaRenderFromProskomma({ proskomma: pk, actions })
   const docId = pk.gqlQuerySync("{documents {id}}").data.documents[0].id
   cl.renderDocument({ docId, config: {}, output })
-  return output.sentences.map((sentence) =>
-    sentence.map((sent) =>
-      sent.map((s) => {
-        return {
-          ...s,
-          source: s.source.map((src, index) => {
-            return {
-              ...src,
-              id: `item-${index}`,
-            }
-          }),
-        }
-      })
-    )
-  )
-  // return output.sentences
+  console.log(output.sentences)
+  return output.sentences
 }
